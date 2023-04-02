859f9585e81c52819de30099dc9a7bf973b09edc
Dad <107422850+dad84@users.noreply.github.com>
2023-04-02 18:57:30 +0200

    moving average calculation and additional symbol information
    This update improves the code's functionality by adding a moving average calculation based on a specified window, and including the moving average and current price information in the potential symbols dictionary. The filter_symbols_with_potential_volume_increase function was updated to filter only symbols ending with 'BTC', and the calculate_moving_average function was added to calculate the moving average of the closing prices for a given symbol and interval. The main function was updated to print the moving average and current price information, in addition to the symbol, volume change percentage, price change percentage, price direction, and exchange link for each potential symbol. These updates provide more comprehensive information for traders looking for potential trading opportunities based on volume increases and moving averages.

--- Volume/volume.py
+++ Volume/volume.py
@@ -5,12 +5,16 @@ import time
 # Set the Binance API endpoint
 api_url = "https://api.binance.com"
 ticker_24hr_endpoint = "/api/v3/ticker/24hr"
+klines_endpoint = "/api/v3/klines"
 
 # Set the minimum volume increase percentage (e.g., 10%)
 min_volume_increase_pct = 10
 
 # Set the time interval between fetches in seconds (e.g., 60 seconds)
 fetch_interval = 60
+
+# Set the moving average window (e.g., 20 periods)
+moving_average_window = 20
 
 def fetch_24hr_ticker_price_change():
     response = requests.get(api_url + ticker_24hr_endpoint)
@@ -20,7 +24,29 @@ def fetch_24hr_ticker_price_change():
         print("Failed to fetch data from Binance API")
         return []
 
-def filter_symbols_with_potential_volume_increase(ticker_data, previous_ticker_data, threshold):
+def fetch_klines_data(symbol, interval):
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": moving_average_window
+    }
+    response = requests.get(api_url + klines_endpoint, params=params)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        print(f"Failed to fetch klines data for {symbol}")
+        return []
+
+def calculate_moving_average(symbol, interval):
+    klines_data = fetch_klines_data(symbol, interval)
+    if not klines_data:
+        return None
+
+    closing_prices = [float(kline[4]) for kline in klines_data]
+    moving_average = sum(closing_prices) / len(closing_prices)
+    return moving_average
+
+def filter_symbols_with_potential_volume_increase(ticker_data, previous_ticker_data, threshold, interval):
     potential_symbols = []
 
     if not previous_ticker_data:
@@ -32,7 +58,7 @@ def filter_symbols_with_potential_volume_increase(ticker_data, previous_ticker_d
         symbol = prev_ticker['symbol']
         prev_quote_volume = float(prev_ticker['quoteVolume'])
 
-        if symbol in ticker_data_dict:
+        if symbol in ticker_data_dict and symbol.endswith('BTC'):  # Only consider symbols ending with 'BTC'
             current_ticker = ticker_data_dict[symbol]
             current_quote_volume = float(current_ticker['quoteVolume'])
 
@@ -40,27 +66,39 @@ def filter_symbols_with_potential_volume_increase(ticker_data, previous_ticker_d
                 volume_change_pct = (current_quote_volume - prev_quote_volume) / prev_quote_volume * 100
 
                 if volume_change_pct >= threshold:
-                    potential_symbols.append({
-                        'symbol': symbol,
-                        'volume_change_pct': volume_change_pct,
-                        'price_change_pct': float(current_ticker['priceChangePercent'])
-                    })
+                    price_change_pct = float(current_ticker['priceChangePercent'])
+                    price_direction = "Up" if price_change_pct > 0 else "Down"
+
+                    moving_average = calculate_moving_average(symbol, interval)
+                    current_price = float(current_ticker['lastPrice'])
+
+                    if moving_average and current_price > moving_average:
+                        potential_symbols.append({
+                            'symbol': symbol,
+                            'volume_change_pct': volume_change_pct,
+                            'price_change_pct': price_change_pct,
+                            'price_direction': price_direction,
+                            'moving_average': moving_average,
+                            'current_price': current_price,
+                            'exchange_link': f"https://www.binance.com/en/trade/{symbol}"
+                        })
 
     return potential_symbols
 
 def main():
     previous_ticker_data = []
+    interval = "1h"  # Set the interval for the moving average calculation (e.g., 1 hour)
 
     while True:
         current_ticker_data = fetch_24hr_ticker_price_change()
-        potential_symbols = filter_symbols_with_potential_volume_increase(current_ticker_data, previous_ticker_data, min_volume_increase_pct)
+        potential_symbols = filter_symbols_with_potential_volume_increase(current_ticker_data, previous_ticker_data, min_volume_increase_pct, interval)
 
         if potential_symbols:
-            print("Symbols with potential volume increase:")
+            print("Symbols with potential volume increase and above moving average:")
             for symbol in potential_symbols:
-                print(f"Symbol: {symbol['symbol']}, Volume Change: {symbol['volume_change_pct']}%, Price Change: {symbol['price_change_pct']}%")
+                print(f"Symbol: {symbol['symbol']}, Volume Change: {symbol['volume_change_pct']}%, Price Change: {symbol['price_change_pct']}%, Direction: {symbol['price_direction']}, Moving Average: {symbol['moving_average']}, Current Price: {symbol['current_price']}, Exchange Link: {symbol['exchange_link']}")
         else:
-            print("No symbols with potential volume increase found")
+            print("No symbols with potential volume increase and above moving average found")
 
         previous_ticker_data = current_ticker_data
         print(f"Waiting {fetch_interval} seconds before the next fetch...")
