d04325c98b4aeba6121acb86b5825097bf69eda6
Dad <107422850+dad84@users.noreply.github.com>
2023-04-05 09:35:04 +0200

    huge update

--- Volume/volume.py
+++ Volume/volume.py
@@ -1,39 +1,36 @@
+# This script fetches 24-hour ticker price change statistics from the Binance API, filters symbols
+# with potential volume increase above a given threshold, calculates the moving average for each symbol,
+# and generates an HTML report with potential buy signals.
 import requests
 import json
 import time
 import os
-
-# Additional imports
 from datetime import datetime
+from ta.momentum import RSIIndicator
+from ta.trend import MACD
+from ta.utils import dropna
+import pandas as pd
 
 # Set the Binance API endpoint
 api_url = "https://api.binance.com"
 ticker_24hr_endpoint = "/api/v3/ticker/24hr"
 klines_endpoint = "/api/v3/klines"
 
-# Set the minimum volume increase percentage (e.g., 10%)
 min_volume_increase_pct = 10
-
-# Set the time interval between fetches in seconds (e.g., 10 seconds)
 fetch_interval = 10
-
-# Set the moving average window (e.g., 20 periods)
 moving_average_window = 20
+rsi_lower_threshold = 30
+macd_cross_threshold = 0
 
-def fetch_24hr_ticker_price_change():
-    response = requests.get(api_url + ticker_24hr_endpoint)
-    if response.status_code == 200:
-        return json.loads(response.text)
-    else:
-        print("Failed to fetch data from Binance API")
-        return []
-
-def fetch_klines_data(symbol, interval):
+# Fetch klines (candlestick) data for a symbol from the Binance API
+def fetch_klines_data(symbol, interval, limit=None):
     params = {
-        "symbol": symbol,
-        "interval": interval,
-        "limit": moving_average_window
+       "symbol": symbol,
+       "interval": interval,
     }
+    if limit is not None:
+        params["limit"] = limit
+
     response = requests.get(api_url + klines_endpoint, params=params)
     if response.status_code == 200:
         return json.loads(response.text)
@@ -41,6 +38,51 @@ def fetch_klines_data(symbol, interval):
         print(f"Failed to fetch klines data for {symbol}")
         return []
 
+def calculate_rsi(symbol, interval, window):
+    klines_data = fetch_klines_data(symbol, interval, window + 1)
+    if not klines_data:
+        return None
+
+    closing_prices = [float(kline[4]) for kline in klines_data]
+    df = pd.DataFrame(closing_prices, columns=["close"])
+    rsi = RSIIndicator(df["close"], window)
+    return rsi.rsi().iloc[-1]
+
+def calculate_macd_cross(symbol, interval, window):
+    klines_data = fetch_klines_data(symbol, interval, window * 3)
+    if not klines_data:
+        return None
+
+    closing_prices = [float(kline[4]) for kline in klines_data]
+    df = pd.DataFrame(closing_prices, columns=["close"])
+    df.dropna(inplace=True)  # Drop NaN values from the DataFrame
+    macd = MACD(df["close"], window_slow=window, window_fast=window // 2, window_sign=window // 4)
+    macd_diff = macd.macd_diff().iloc[-1]
+
+    return macd_diff
+
+def check_potential_price_increase(symbol, interval):
+    rsi = calculate_rsi(symbol, interval, moving_average_window)
+    macd_cross = calculate_macd_cross(symbol, interval, moving_average_window)
+
+    if rsi is None or macd_cross is None:
+        return False
+
+    if rsi <= rsi_lower_threshold and macd_cross > macd_cross_threshold:
+        return True
+
+    return False
+    
+# Fetch 24-hour ticker price change statistics from the Binance API    
+def fetch_24hr_ticker_price_change():
+    response = requests.get(api_url + ticker_24hr_endpoint)
+    if response.status_code == 200:
+        return json.loads(response.text)
+    else:
+        print("Failed to fetch data from Binance API")
+        return []
+    
+# Calculate the moving average for a symbol based on the given klines data
 def calculate_moving_average(symbol, interval):
     klines_data = fetch_klines_data(symbol, interval)
     if not klines_data:
@@ -89,6 +131,24 @@ def filter_symbols_with_potential_volume_increase(ticker_data, previous_ticker_d
 
     return potential_symbols
 
+def filter_symbols_with_potential_price_increase(ticker_data, interval):
+    potential_symbols = []
+
+    for ticker in ticker_data:
+        symbol = ticker['symbol']
+
+        if symbol.endswith('BTC'):  # Only consider symbols ending with 'BTC'
+            if check_potential_price_increase(symbol, interval):
+                current_price = float(ticker['lastPrice'])
+                potential_symbols.append({
+                    'symbol': symbol,
+                    'current_price': current_price,
+                    'exchange_link': f"https://www.binance.com/en/trade/{symbol}"
+                })
+
+    return potential_symbols
+
+# Fetch coin logo URLs from the CoinGecko API
 def fetch_coin_logo_urls():
     coingecko_api_url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {"vs_currency": "btc", "order": "market_cap_desc", "per_page": 200, "page": 1, "sparkline": False}
@@ -226,33 +286,40 @@ def generate_html_content(last_5_signals, logo_urls):
 </html>
 """
     return html_content
-
+    
+# Save the generated HTML content to a file
 def save_html_file(html_content, file_name="potential_buy_signals.html"):
     with open(file_name, "w") as f:
         f.write(html_content)
-
+        
+# Main function to fetch data, filter symbols, and generate the HTML report
 def main():
-    previous_ticker_data = []
     interval = "1h"
     last_5_signals = []
     logo_urls = fetch_coin_logo_urls()
 
+    previous_ticker_data = []
+
     while True:
         current_ticker_data = fetch_24hr_ticker_price_change()
-        potential_symbols = filter_symbols_with_potential_volume_increase(current_ticker_data, previous_ticker_data, min_volume_increase_pct, interval)
+        potential_symbols = filter_symbols_with_potential_volume_increase(
+            current_ticker_data, previous_ticker_data, min_volume_increase_pct, interval
+        )
 
         if potential_symbols:
             last_5_signals.append(potential_symbols)
             last_5_signals = last_5_signals[-5:]
             html_content = generate_html_content(last_5_signals, logo_urls)
             save_html_file(html_content)
-            print("Saved potential buy signals to HTML file")
+            print("Saved potential price increase signals to HTML file")
         else:
-            print("No symbols with potential volume increase and above moving average found")
+            print("No symbols with potential price increase found")
 
         previous_ticker_data = current_ticker_data
+
         print(f"Waiting {fetch_interval} seconds before the next fetch...")
         time.sleep(fetch_interval)
-
+        
+# Run the main function when the script is executed
 if __name__ == "__main__":
     main()
